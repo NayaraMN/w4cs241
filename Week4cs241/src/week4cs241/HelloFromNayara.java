00be3954aceafa7b52c67fa0c6b66609a7c27973
NayaraMN <nayara.mnobre@gmail.com>
2021-10-09 22:22:19 -0300

    HelloFromNayara 1

---
+++ Week4cs241/src/week4cs241/HelloFromNayara.java
@@ -0,0 +1,11 @@
+package week4cs241;
+
+public class HelloFromNayara {
+
+    public static void main(String[] args) {
+        String message = "Hello my name is Nayara";
+        System.out.println(message);
+
+     }
+
+}
